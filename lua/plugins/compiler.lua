856d8a4ea3f0ddfc0cfeb6968404aeea6b013e28
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-07-23 18:55:58 -0300

    compiler?

--- lua/plugins/compiler.lua
+++ lua/plugins/compiler.lua
@@ -1,23 +1,6 @@
 return { -- This plugin
     "Zeioth/compiler.nvim",
-        cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-        dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
-        opts = {},
-    },
-    { -- The task runner we use
-        "stevearc/overseer.nvim",
-        commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
-        cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-        opts = {
-            task_list = {
-                direction = "bottom",
-                min_height = 30,
-                max_height = 30,
-                default_detail = 1
-            },
-        },
-
-        config = function ()
-            require("overseer").setup()
-        end
-    }
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {},
+}
