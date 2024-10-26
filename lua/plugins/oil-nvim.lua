45f09e8225406f539718bb42540c7d265665522a
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-10-25 21:23:34 -0300

    melhoramento na configuração do oil.nvim

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -1,5 +1,10 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+        skip_confirm_for_simple_edits = true,
+        -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+        -- (:help prompt_save_on_select_new_entry)
+        prompt_save_on_select_new_entry = false,
+    },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
 }
