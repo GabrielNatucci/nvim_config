21eacfe97673c89e22eca46658677af2c56a9e61
Gabriel Natucci <gabriel.natuccirusso@hotmail.com>
2025-09-12 19:44:05 -0300

    Mais trela

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,4 +1,44 @@
 return {
---     "danymat/neogen", 
---     config = true,
+    "danymat/neogen",
+    config = function()
+        require("neogen").setup({
+            enabled = true,
+            languages = {
+                java = {
+                    template = {
+                        class = {
+                            [[
+/**
+ * Class: ${name}
+ * Description: ${description}
+ *
+ * @author ${author}
+ * @date ${date}
+*/
+                    ]]
+                        },
+                        type = {
+                            [[
+/**
+ * Type: ${name}
+ * Description: ${description}
+*/
+                    ]]
+                        },
+                        method = {
+                            [[
+/**
+ * Method: ${name}
+ * Description: ${description}
+ *
+ * @param ${param} ${param_desc}
+ * @return ${return_desc}
+*/
+                    ]]
+                        }
+                    }
+                }
+            }
+        })
+    end,
 }
